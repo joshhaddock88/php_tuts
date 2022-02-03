d857625f588ef4d2d25039d5f91340312f62a8f6
Joshua Haddock <joshhaddock88@gmail.com>
2022-02-03 08:06:21 -0800

    Variables and Constants, tuts4 complete

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php 
 
 // echo 'Hello, World!';
+// variables (can't start with number or special character. Should start with lower case)
+
+//define function is used to create a constant.
+// by convention, constant names are all caps
+define('NAME', 'Yoshi');
+
+// $variable to define a non-constant variable
+// $name = 'Yoshi';
+$age = 30;
+
+// name can be overridden in this instance, because it is non-constant
+// $name = 'Mario';
+
 
 ?>
 
@@ -11,5 +24,8 @@
     </head>
     <body>
         <h1>User Profile Pages</h1>
+
+        <div><?php echo NAME ?></div>
+        <div><?php echo $age ?></div>
     </body>
 </html>
