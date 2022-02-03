c0c04794247cc5aab0880dcf63549ea767d02104
Joshua Haddock <joshhaddock88@gmail.com>
2022-02-03 07:56:12 -0800

    First bit of PHP Codoe, tutorial #3

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// echo 'Hello, World!';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1><?php echo 'Hello, World!' ?></h1>
+    </body>
+</html>
